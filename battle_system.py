a8f2c32f8d5bc2aaecd8f3d93c20fc10d745c494
Nafiq06 <nafiq.hoque@elev.ga.ntig.se>
2025-04-01 10:27:50 +0200

    Add files via upload
    minor update on battle system

--- battle_system.py
+++ battle_system.py
@@ -9,3 +9,15 @@
 import random
 print("you're about to fight the guard, get ready fight starts now!")
 print("player goes first, lets roll a dice if you get a hit or not")
+
+
+round1 = random.randint(1,100)
+round2 = random.randint(1,100) # 1, 2, 3, 4, 5, 6
+round3 = random.randint(1,100)
+
+print("round 1 visar: " + str(round1))
+print(f"round 2 visar: {round2}")
+print("round 3 visar: " + str(round3))
+
+result = (round1 + round2 + round3)
+print("Resultaten är : ", result)
