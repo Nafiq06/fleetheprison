2daf20010591c474baed686fbc1dc4f212796011
Nafiq06 <nafiq.hoque@elev.ga.ntig.se>
2025-04-03 12:58:20 +0200

    Add files via upload

--- battle_system.py
+++ battle_system.py
@@ -8,16 +8,25 @@
 
 import random
 print("you're about to fight the guard, get ready fight starts now!")
-print("player goes first, lets roll a dice if you get a hit or not")
+print("player goes first,lets see if you get a hit or not")
 
 
-round1 = random.randint(1,100)
-round2 = random.randint(1,100) # 1, 2, 3, 4, 5, 6
-round3 = random.randint(1,100)
+# round1 = random.randint(1,100)
+# round2 = random.randint(1,100) # 1, 2, 3, 4, 5, 6
+# round3 = random.randint(1,100)
 
-print("round 1 visar: " + str(round1))
-print(f"round 2 visar: {round2}")
-print("round 3 visar: " + str(round3))
+# print("round 1 show: " + str(round1))
+# print(f"round 2 show: {round2}")
+# print("round 3 show: " + str(round3))
 
-result = (round1 + round2 + round3)
-print("Resultaten är : ", result)
+# result = (round1 + round2 + round3)
+# print("End result : ", result)
+
+Enemyhp= 100
+while Enemyhp >=0:
+    round = random.randint(1,100)
+    Hit = 50
+    if round >= 50:
+        round = Hit
+        round - Enemyhp
+        print(Enemyhp, "remaining")
